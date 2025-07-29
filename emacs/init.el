745f259915b9aaedafa8a85e5e158039044eb0ac
Luis <luis@kodebay.com>
2025-07-29 01:57:31 +0100

    Improvements to emacs

--- emacs/init.el
+++ emacs/init.el
@@ -27,7 +27,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(gptel org-roam rust-mode slime tree-sitter-langs vertico)))
+ '(package-selected-packages
+   '(consult gptel magit multiple-cursors orderless org-roam rust-mode
+	     slime tree-sitter-langs vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
